3bfb37466f077a0a0d09b30b654692369e4de72a
sujen07 <sujenk@gmail.com>
2024-08-01 14:09:43 -0700

    add capability with images and text

--- github_to_weaviate_embedder.py
+++ github_to_weaviate_embedder.py
@@ -1,5 +1,13 @@
 import requests
 import pdb
+import base64
+
+from weaviate_init import client
+
+git_files_collection = client.collections.get('GitFiles')
+
+def toBase64(content):
+    return base64.b64encode(content).decode('utf-8')
 
 def process_github_repo(owner, repo, path='', process_function=None):
     api_url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
@@ -27,15 +35,32 @@ def process_github_repo(owner, repo, path='', process_function=None):
         print(f"Failed to retrieve repository contents. Status code: {response.status_code}")
 
 # Example processing function
-def example_process_function(filename, content):
+def add_to_embedding_collection(filename, content):
     # This is a placeholder function. Replace it with your actual processing logic.
     print(f"Processing file: {filename}")
     print(f"File size: {len(content)} bytes")
-    pdb.set_trace()
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+        git_files_collection.data.insert(
+            {
+                "name": filename,
+                "image": toBase64(content),
+                "mediaType": "image",
+            }
+        )
+    else:
+        git_files_collection.data.insert(
+            {
+                "name": filename,
+                "text": toBase64(content),
+                "mediaType": "text",
+            }
+        )
+
     # Add your processing logic here
     # For example, you could parse the content, extract information, or perform any other operation
 
 # Example usage
-owner = 'kashish-ag'
-repo = 'Detection-of-Plant-Disease'
-process_github_repo(owner, repo, process_function=example_process_function)
+owner = 'sujen07'
+repo = 'test-repo'
+process_github_repo(owner, repo, process_function=add_to_embedding_collection)
+client.close()
